4b3b11b5f8a354b4117156707176c8ee1fe72f94
Sam Carr <sam.carr@kayako.com>
2015-06-10 22:13:32 +0100

    mobile basic setup

--- app/models/drink.js
+++ app/models/drink.js
@@ -3,14 +3,15 @@ import DS from 'ember-data';
 var drink = DS.Model.extend({
   name: DS.attr('string'),
   img_url: DS.attr('string'),
-  description: DS.attr('string')
+  description: DS.attr('string'),
+  subtitle: DS.attr('string')
 });
 
 drink.reopenClass({
 	FIXTURES: [
 		{
 			id: "apple-and-elderflower",
-			name: "Apple & Elderflower",
+			name: "APPLE & ELDERFLOWER",
 			img_url: "/images/AppleElderflower.png",
 			description:
 				'Influenced by English summer flavours, a very light and refreshing drink. '
@@ -18,19 +19,20 @@ drink.reopenClass({
 		},
     {
       id: "orange-and-mango",
-      name: "Orange & Mango",
+      name: "ORANGE & MANGO",
       img_url: "/images/OrangeMango.png",
       description: 'An exotic take on a Sam and Jo\'sq childhood favourite.'
     },
 		{
 			id: "couldy-lemon",
-			name: "Cloudy Lemon",
+			name: "CLOUDY LEMON",
+      subtitle: "with a hint of Ginger",
 			img_url: "/images/CloudyLemon.png",
       description: 'A traditional classic with some additional spice.'
 		},
     {
       id: "cranberry-and-lime",
-      name: "Cranberry & Lime",
+      name: "CRANBERRY & LIME",
       img_url: "/images/CranberryLime.png",
       description: 'Berry sweetness with a distinctive after bite!'
     }
